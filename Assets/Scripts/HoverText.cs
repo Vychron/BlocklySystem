c3e9611ba67cd293ea06740a6203d54db77f034c
Vychron <demi.a.joseph@gmail.com>
2020-03-30 09:42:58 +0200

    Add HoverText description Additions: - HoverText now has XML summaries.

--- Assets/Scripts/HoverText.cs
+++ Assets/Scripts/HoverText.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// HoverText is a singleton text object that is used to show text when hovering over certain objects.
+/// </summary>
 public class HoverText : MonoBehaviour
 {
+    /// <summary>
+    /// A static reference to the component.
+    /// </summary>
     public static HoverText Instance;
 
     private void Start()
